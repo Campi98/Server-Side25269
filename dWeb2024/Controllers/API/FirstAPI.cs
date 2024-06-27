5dec796d1bc9cf9e1c35f5809a003b845481caef
Campi98 <121031836+Campi98@users.noreply.github.com>
2024-06-27 18:14:29 +0100

    +API avaliacoes; perfis

--- dWeb2024/Controllers/API/FirstAPI.cs
+++ dWeb2024/Controllers/API/FirstAPI.cs
@@ -98,5 +98,175 @@ namespace dWeb2024.Controllers.API
         {
             return _context.Users.Any(e => e.Id_do_User == id);
         }
+
+        // ############################################################################################################
+
+        // GET: api/FirstAPI/perfis
+        [HttpGet("perfis")]
+        public async Task<ActionResult<IEnumerable<Perfil>>> GetPerfis()
+        {
+            return await _context.Perfis.ToListAsync();
+        }
+
+        // GET: api/FirstAPI/perfis/5
+        [HttpGet("perfis/{id}")]
+        public async Task<ActionResult<Perfil>> GetPerfil(int id)
+        {
+            var perfil = await _context.Perfis.FindAsync(id);
+
+            if (perfil == null)
+            {
+                return NotFound();
+            }
+
+            return perfil;
+        }
+
+        // POST: api/FirstAPI/perfis
+        [HttpPost("perfis")]
+        public async Task<ActionResult<Perfil>> PostPerfil(Perfil perfil)
+        {
+            _context.Perfis.Add(perfil);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerfil", new { id = perfil.Id_do_Perfil }, perfil);
+        }
+
+        // PUT: api/FirstAPI/perfis/5
+        [HttpPut("perfis/{id}")]
+        public async Task<IActionResult> PutPerfil(int id, Perfil perfil)
+        {
+            if (id != perfil.Id_do_Perfil)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(perfil).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PerfilExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/FirstAPI/perfis/5
+        [HttpDelete("perfis/{id}")]
+        public async Task<IActionResult> DeletePerfil(int id)
+        {
+            var perfil = await _context.Perfis.FindAsync(id);
+            if (perfil == null)
+            {
+                return NotFound();
+            }
+
+            _context.Perfis.Remove(perfil);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PerfilExists(int id)
+        {
+            return _context.Perfis.Any(e => e.Id_do_Perfil == id);
+        }
+
+
+        // ############################################################################################################
+
+        // GET: api/FirstAPI/avaliacoes
+        [HttpGet("avaliacoes")]
+        public async Task<ActionResult<IEnumerable<Avaliacao>>> GetAvaliacoes()
+        {
+            return await _context.Avaliacoes.ToListAsync();
+        }
+
+        // GET: api/FirstAPI/avaliacoes/5
+        [HttpGet("avaliacoes/{id}")]
+        public async Task<ActionResult<Avaliacao>> GetAvaliacao(int id)
+        {
+            var avaliacao = await _context.Avaliacoes.FindAsync(id);
+
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            return avaliacao;
+        }
+
+        // POST: api/FirstAPI/avaliacoes
+        [HttpPost("avaliacoes")]
+        public async Task<ActionResult<Avaliacao>> PostAvaliacao(Avaliacao avaliacao)
+        {
+            _context.Avaliacoes.Add(avaliacao);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAvaliacao", new { id = avaliacao.Id_da_Avaliacao }, avaliacao);
+        }
+
+        // PUT: api/FirstAPI/avaliacoes/5
+        [HttpPut("avaliacoes/{id}")]
+        public async Task<IActionResult> PutAvaliacao(int id, Avaliacao avaliacao)
+        {
+            if (id != avaliacao.Id_da_Avaliacao)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(avaliacao).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AvaliacaoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/FirstAPI/avaliacoes/5
+        [HttpDelete("avaliacoes/{id}")]
+        public async Task<IActionResult> DeleteAvaliacao(int id)
+        {
+            var avaliacao = await _context.Avaliacoes.FindAsync(id);
+            if (avaliacao == null)
+            {
+                return NotFound();
+            }
+
+            _context.Avaliacoes.Remove(avaliacao);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AvaliacaoExists(int id)
+        {
+            return _context.Avaliacoes.Any(e => e.Id_da_Avaliacao == id);
+        }
+
     }
 }
