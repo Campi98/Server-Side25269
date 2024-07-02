f958cafab5c745e66de2a886e6b1a9a612c166c2
Campi98 <121031836+Campi98@users.noreply.github.com>
2024-07-02 23:42:19 +0100

    fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# WebDev_Eq2grau
-Aplicação para determinar raízes de equação de 2º grau.
--- WebDev_Eq2grau/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebDev_Eq2grau.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
